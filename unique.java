171e14bd7c9da39850e06a8b6859d17f9ea73730
BharathBora <115933504+BharathBora@users.noreply.github.com>
2023-02-12 18:45:22 +0530

    Add files via upload

---
+++ unique.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class unique {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,j;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<i;j++)
+            {
+                if(x[i]==x[j])
+                break;
+        }
+        if(i==j)
+        System.out.print(x[i]+" ");
+    }
+        
+        }
+    
+}
